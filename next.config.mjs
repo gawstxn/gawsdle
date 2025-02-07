99ea8a1790afebb40e42cc3bf3fcf26f9101df94
Naruenat Khamwaree <95065379+gawston@users.noreply.github.com>
2025-02-07 13:44:26 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['enka.network', 'raw.githubusercontent.com', 'api.yatta.top', 'genshin.jmp.blue'],
+        domains: [
+            'enka.network', 
+            'raw.githubusercontent.com', 
+            'api.yatta.top', 
+            'genshin.jmp.blue', 
+            'keqingmains.com'
+        ],
     },
 };
 
